f6a2429c44197a14ae6c6a44d1ac243300162294
Shubh Patel <shubhp18@gmail.com>
2021-06-24 09:01:22 -0400

    Rewrite/Restructuring of Website

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,27 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE)
     */
-    (function ($) {
+
+(function ($) {
     "use strict"; // Start of use strict
 
-    // Smooth scrolling using jQuery easing
+    // Smooth scrolling
     $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
         if (
-            location.pathname.replace(/^\//, "") ==
-                this.pathname.replace(/^\//, "") &&
-            location.hostname == this.hostname
+            location.pathname.replace(/^\//, "") ===
+            this.pathname.replace(/^\//, "") &&
+            location.hostname === this.hostname
         ) {
-            var target = $(this.hash);
+            let target = $(this.hash);
             target = target.length
                 ? target
                 : $("[name=" + this.hash.slice(1) + "]");
             if (target.length) {
-                $("html, body").animate(
-                    {
-                        scrollTop: target.offset().top - 72,
-                    },
-                    1000,
-                    "easeInOutExpo"
-                );
+                target[0].scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start',
+                    inline: 'start'
+                })
                 return false;
             }
         }
@@ -36,21 +35,25 @@
     });
 
     // Activate scrollspy to add active class to navbar items on scroll
-    $("body").scrollspy({
+    $("#page-container").scrollspy({
         target: "#mainNav",
         offset: 74,
     });
 
     // Collapse Navbar
-    var navbarCollapse = function () {
-        if ($("#mainNav").offset().top > 100) {
-            $("#mainNav").addClass("navbar-shrink");
+    const navbarCollapse = function () {
+        const mainNav = $("#mainNav");
+        if (mainNav.offset().top > 100) {
+            mainNav.addClass("navbar-shrink");
         } else {
-            $("#mainNav").removeClass("navbar-shrink");
+            mainNav.removeClass("navbar-shrink");
         }
     };
+
     // Collapse now if page is not at top
     navbarCollapse();
+
     // Collapse the navbar when page is scrolled
     $(window).scroll(navbarCollapse);
+
 })(jQuery); // End of use strict
